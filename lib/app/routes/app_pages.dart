679b60b5e8dd0583bbbb71026db465b9ee4d713f
andrius <a.riauba@gmail.com>
2021-08-01 15:06:18 +0300

    scelet created

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
-
+import 'package:money_calculator_v2/app/modules/auth/bindings/auth_binding.dart';
+import 'package:money_calculator_v2/app/modules/auth/views/auth_view.dart';
 import 'package:money_calculator_v2/app/modules/home/bindings/home_binding.dart';
 import 'package:money_calculator_v2/app/modules/home/views/home_view.dart';
 
@@ -15,6 +16,11 @@ class AppPages {
       name: _Paths.HOME,
       page: () => HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.AUTH,
+      page: () => AuthView(),
+      binding: AuthBinding(),
     ),
   ];
 }
